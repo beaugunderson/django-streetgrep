232b0782dadbc2e57a95f984ce98ffaabfa2a1b2
Beau Gunderson <beau@beaugunderson.com>
2013-12-18 21:55:49 -0800

    Add taggit

--- art/models.py
+++ art/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from taggit.managers import TaggableManager
+
 class Picture(models.Model):
     uploader = models.ForeignKey(User)
-    description = models.CharField(max_length=256)
 
 class Piece(models.Model):
+    description = models.CharField(max_length=256)
     pictures = models.ManyToManyField(Picture)
+    tags = TaggableManager()
+
+    # location: geo
